d72dcea496823a640fb5786efc06527a8d632cea
hadi-h82 <hadi.deris.h82@gmail.com>
2022-05-01 17:06:28 +0430

    added app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'form-project';
+
+  constructor(private dialog: MatDialog , private api: ApiService) {}
+
+
+  ngOnInit(): void {
+    this.getAllProduct();
+  }
+
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      width: '30%' ,
+      height: '70%'
+    });
+  }
+
+
+  getAllProduct() {
+    this.api.getProduct()
+      .subscribe({
+        next:(res)=>{
+          console.log(res);
+        },
+        error:()=> {
+          alert("Error while fetching the Records!!!")
+        }
+      })
+
+  }
+
+
+
 }
+
+// npx json-server --watch db.json
