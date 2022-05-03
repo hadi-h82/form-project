e6bf7c3124ce4609a098767090599922f3f5fa67
hadi-h82 <hadi.deris.h82@gmail.com>
2022-05-03 16:03:40 +0430

    added share modules

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,66 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+export const components: any = [];
+export const directives: any = [];
+export const UIModule = [
+  MatDialogModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatRadioModule,
+  MatToolbarModule,
+  MatSelectModule,
+  MatCardModule,
+  MatGridListModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatTabsModule,
+  MatListModule,
+  MatTooltipModule,
+  MatInputModule,
+  MatCheckboxModule,
+  MatMenuModule,
+  MatIconModule,
+  MatProgressSpinnerModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatSliderModule,
+];
+export const modules = [
+  ReactiveFormsModule,
+  HttpClientModule,
+  BrowserAnimationsModule,
+  ToastrModule.forRoot(),
+];
+@NgModule({
+  imports: [...modules, ...UIModule],
+  declarations: [...components, ...directives],
+  exports: [...components, ...modules, ...UIModule, ...directives],
+  providers: [],
+})
+export class SharedModule {}
